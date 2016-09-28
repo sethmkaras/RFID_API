2cc1f96b9b8e7701077bb4a48c1d4cb511bdf9f6
Matthew Howard <mthwoard@buffalo.edu>
2016-09-27 22:12:05 -0500

    Total change

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 from app import app
 import os
-port = int(os.environ.get('PORT', 9000))
+port = int(os.environ.get('PORT', 5000))
 #app.run(host='192.168.1.159', port=5000)
-app.run(debug=True, port=port)
+app.run(host='0.0.0.0', port=port)
