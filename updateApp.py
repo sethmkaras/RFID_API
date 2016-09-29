25182b13232da63b65159101ee17b75befbac841
Matthew Howard <mthwoard@buffalo.edu>
2016-09-28 20:39:18 -0500

    scanner related

--- updateApp.py
+++ updateApp.py
@@ -3,7 +3,3 @@ import os, time
 os.system('git add *')
 os.system('git commit -m "scanner related"')
 os.system('git push -u origin master');
-time.sleep(1)
-os.system('sethmkaras')
-time.sleep(0.5)
-os.system('Altima61!')
